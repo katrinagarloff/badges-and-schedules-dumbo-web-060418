e799ded61621700a768e1db98c4907c0e6cb3ea4
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-17 20:59:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def batch_badge_creator(names)
   badges = []
   
   names.each do |name|
-    badges.push("Hello, my name is #{name}")
+    badges.push("Hello, my name is #{name}.")
   end
   return badges
 end
